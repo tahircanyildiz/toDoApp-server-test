88ffe9d6b1a6eece2f63537dcc5c00e9026095ec
tahircanyildiz <tahircanyildiz@hotmail.com>
2024-07-22 13:43:50 +0300

    modified

--- cypress/e2e/todoTest.cy.js
+++ cypress/e2e/todoTest.cy.js
@@ -23,48 +23,40 @@ describe('Login Test', () => {
       const updatedTodoText1 = 'Güncellenmiş Görev 1';
 
       const newTodoText2 = 'Yeni Görev 2';
-      const updatedTodoText2 = 'Güncellenmiş Görev 2';
 
       const newTodoText3 = 'Yeni Görev 3';
-      const updatedTodoText3 = 'Güncellenmiş Görev 3';
 
       const newTodoText4 = 'Yeni Görev 4';
-      const updatedTodoText4 = 'Güncellenmiş Görev 4';
-
 
       cy.get("input#new-todo").type(newTodoText1);
       cy.get("div#todo-app div.todo-input.todo-item > button > i").click();
+
       cy.get("input#new-todo").type(newTodoText2);
       cy.get("div#todo-app div.todo-input.todo-item > button > i").click();
+
       cy.get("input#new-todo").type(newTodoText3);
       cy.get("div#todo-app div.todo-input.todo-item > button > i").click();
+
       cy.get("input#new-todo").type(newTodoText4);
       cy.get("div#todo-app div.todo-input.todo-item > button > i").click();
+      
       //to-do statü değiştirme kısmı
       cy.contains(newTodoText1).should('exist');
       cy.contains(newTodoText1).parent().find('.status-select').select('Devam Ediyor');
       cy.contains(newTodoText1).parent().find('.status-select').should('have.value', 'Devam Ediyor');
+
       cy.contains(newTodoText1).parent().find('.status-select').select('Tamamlandı');
       cy.contains(newTodoText1).parent().find('.status-select').should('have.value', 'Tamamlandı');
-
-         cy.contains(newTodoText2).should('exist');
-         cy.contains(newTodoText2).parent().find('.status-select').select('Devam Ediyor');       
-         cy.contains(newTodoText2).parent().find('.status-select').should('have.value', 'Devam Ediyor');
+      
+      cy.contains(newTodoText2).should('exist');
+      cy.contains(newTodoText2).parent().find('.status-select').select('Devam Ediyor');       
+      cy.contains(newTodoText2).parent().find('.status-select').should('have.value', 'Devam Ediyor');
  
 
-         //to-do güncelleme
-         cy.get('#todo-list').contains(newTodoText1)
-        .parent()
-        .find('.update-btn')
-        .click();
+      //to-do güncelleme
+      cy.get('#todo-list').contains(newTodoText1).parent().find('.update-btn').click();
 
-      cy.get('#todo-list')
-        .find('input[type="text"]')
-        .click()
-        .should('have.value', newTodoText1)
-        .clear()
-        .type(updatedTodoText1)
-        .blur();  
+      cy.get('#todo-list').find('input[type="text"]').click().should('have.value', newTodoText1).clear().type(updatedTodoText1).blur();  
 
       cy.get('#todo-list').contains(updatedTodoText1).should('exist');
 
@@ -72,10 +64,7 @@ describe('Login Test', () => {
 
 
       //to-do silme
-      cy.get('#todo-list').contains(newTodoText4)
-        .parent()
-        .find('.delete-btn')
-        .click();
+      cy.get('#todo-list').contains(newTodoText4).parent().find('.delete-btn').click();
 
       cy.get('#todo-list').contains(newTodoText4).should('not.exist');
       
